28f29f7eb0d638e16a11ee70a4f018555a96f1bd
Jameson2011 <j7db@hotmail.co.uk>
2019-04-13 00:36:13 +0100

    build to multiple runtimes

--- build.fsx
+++ build.fsx
@@ -28,12 +28,20 @@ let publishOptions = fun (opts: DotNet.PublishOptions) ->
                                 { opts with
                                     Configuration = DotNet.BuildConfiguration.Release;
                                  }
+                                 
+let publishOptionsByRuntime = fun (runtime: string) (opts: DotNet.PublishOptions) -> 
+                                        { opts with
+                                            Configuration = DotNet.BuildConfiguration.Release;
+                                            Runtime = Some runtime
+                                         }
+
+let publishProjects = !! "src/**/zKbProxy.fsproj" |> List.ofSeq
 
 Target.create "Clean" (fun _ ->
     !! "src/**/bin"
     ++ "src/**/obj"
     ++ buildDir
-    ++ publishDir
+    ++ publishDir   
     |> Shell.cleanDirs
 )
 
@@ -42,18 +50,35 @@ Target.create "Build" (fun _ ->
     |> Seq.iter (DotNet.build buildOptions)
 )
 
-Target.create "Pack" (fun _ -> !! "src/**/zKbProxy.fsproj" |> Seq.iter (DotNet.pack packOptions ) )
+Target.create "Pack" (fun _ -> publishProjects |> Seq.iter (DotNet.pack packOptions ) )
 
-Target.create "Publish" (fun _ -> !! "src/**/zKbProxy.fsproj" |> Seq.iter (DotNet.publish publishOptions ) )
+Target.create "Publish" (fun _ -> publishProjects |> Seq.iter (DotNet.publish publishOptions ) )
 
 Target.create "CopyPublication" (fun _ -> Shell.copyDir publishDir @"src\zKbProxy\bin\Release\netcoreapp2.2\publish" (fun _ -> true) )
+
+
+let publishAndCopy runtime =
+    publishProjects
+        |> Seq.iter (fun p -> p |> DotNet.publish (publishOptionsByRuntime runtime)) 
+                                      
+    let sourceDir = sprintf @"src\zKbProxy\bin\Release\netcoreapp2.2\%s\publish" runtime
+    let targetDir = sprintf @".\%s\%s" publishDir runtime
+
+    Shell.copyDir targetDir sourceDir (fun _ -> true)
+
+Target.create "PublishRuntime-win-x86" (fun _ -> publishAndCopy "win-x86")
+Target.create "PublishRuntime-win-arm" (fun _ -> publishAndCopy "win-arm")
+Target.create "PublishRuntime-linux-arm" (fun _ -> publishAndCopy "linux-arm")
+Target.create "PublishRuntime-ubuntu-x64" (fun _ -> publishAndCopy "ubuntu-x64")
 
 Target.create "All" ignore
 
 "Clean"
   ==> "Build"
-  ==> "Publish"
-  ==> "CopyPublication"
+  ==> "PublishRuntime-win-x86"
+  ==> "PublishRuntime-win-arm"
+  ==> "PublishRuntime-linux-arm"
+  ==> "PublishRuntime-ubuntu-x64"
   ==> "All"
 
 Target.runOrDefault "All"
